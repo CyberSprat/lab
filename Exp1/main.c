22d1f0300dd90d64865e2877fdf85d04e1017d76
CyberSprat <116829468+CyberSprat@users.noreply.github.com>
2022-10-27 22:34:28 +0500

    Update main.c

--- Exp1/main.c
+++ Exp1/main.c
@@ -44,28 +44,28 @@ void menu(circle v, float *dot1, float *dot2, char *strCmd){
 	switch(i){
 		case 1:
 			checkingDotInZones(v, dot1, strCmd);
-			printf("\nPush any key...);
+			printf("\nPush any key...");
 			key=getch();
 			checkingDotInZones(v, dot2, strCmd);
-			printf("\nPush any key...);
+			printf("\nPush any key...");
 			key=getch();
 			break;
 		case 2:
 			directionChecking(dot1, dot2, strCmd);
-			printf("\nPush any key...);
+			printf("\nPush any key...");
 			key=getch();
 			break;
 		case 3:
 			dotInBorderChecking(v, dot1, strCmd);
-			printf("\nPush any key...);
+			printf("\nPush any key...");
 			key=getch();
 			dotInBorderChecking(v, dot2, strCmd);
-			printf("\nPush any key...);
+			printf("\nPush any key...");
 			key=getch();
 			break;
 		case 4:
 			dotCCheckingOnAB(dot1, dot2, strCmd);
-			printf("\nPush any key...);
+			printf("\nPush any key...");
 			key=getch();
 			break;
 	}
