122600b8f20a80905b739d324a0ae8b1f5746271
CyberSprat <116829468+CyberSprat@users.noreply.github.com>
2022-10-27 22:22:49 +0500

    Update main.c

--- Exp1/main.c
+++ Exp1/main.c
@@ -1,13 +1,61 @@
 #include "ebobo.h"
+#include <conio.h>
+
+void menu(circle v, float *dot1, float *dot2, char *strCmd);
 
 int main(){
 	circle v7;
-	float tempDot1[2]={1,1};
-	float tempDot2[2]={3,3};
+	float Dot1[2], Dot2[2];
+	printf("Enter x and y dot A:");
+	scanf("%f%f", &Dot1[0], &Dot1[1]);
+	printf("Enter x and y dot B:");
+	scanf("%f%f", &Dot2[0], &Dot2[1]);
 	coordZoneA(v7.zoneAX, v7.zoneAY);
 	coordZoneB(v7.zoneBX, v7.zoneBY);
 	coordZoneC(v7.zoneCX, v7.zoneCY);
 	coordZoneD(v7.zoneDX, v7.zoneDY);
-	dotCCheckingOnAB(tempDot1, tempDot2, "clear");
 	return 0;
+}
+
+void menu(circle v, float *dot1, float *dot2, char *strCmd){
+	int key=0, i=1;
+	do{
+		switch(i){
+			case 1:
+				printMenuSelected_checkingDotInZones(strCmd);
+				break;
+			case 2:
+				printMenuSelected_directionChecking(strCmd);
+				break;
+			case 3:
+				printMenuSelected_dotInBorderChecking(strCmd);
+				break;
+			case 4:
+				printMenuSelected_dotCCheckingOnAB(strCmd);
+				break;
+		}
+		key=getch();
+		if(key==115 && i>=1)
+			i++;
+		else if(key==119 && i<=4)
+			i--;
+	}while(key!=13);
+	switch(i){
+		case 1:
+			checkingDotInZones(v, dot1, strCmd);
+			for(int x=0; x<1000; x++);
+			checkingDotInZones(v, dot2, strCmd);
+			break;
+		case 2:
+			directionChecking(dot1, dot2, strCmd);
+			break;
+		case 3:
+			dotInBorderChecking(v, dot1, strCmd);
+			for(int x=0; x<1000; x++);
+			dotInBorderChecking(v, dot2, strCmd);
+			break;
+		case 4:
+			dotCCheckingOnAB(dot1, dot2, strCmd);
+			break;
+	}
 }
